6e5cc8854326056573f5e20998be0cd22e93eecb
Abdallah <shepy@live.com>
2023-11-13 22:36:12 +0200

    Home edits .

--- js/custom.js
+++ js/custom.js
@@ -437,4 +437,41 @@ if (editPhoneInput) {
 
     }
 
+
+// Marquee
+    $('.marquee').marquee({
+
+        // Set to false if you want to use jQuery animate method
+        allowCss3Support: true,
+
+        // CSS3 easing function
+        css3easing: 'linear',
+
+
+        // Time to wait before starting the animation
+        delayBeforeStart: 1000,
+
+        // 'left', 'right', 'up' or 'down'
+        direction: 'left',
+
+        // Should the marquee be duplicated to show an effect of continues flow
+        duplicated: false,
+
+        // Duration of the animation
+        duration: 10000,
+
+        // Space in pixels between the tickers
+        gap: 20,
+
+        // On cycle pause the marquee
+        pauseOnCycle: false,
+
+        // Pause on hover
+        pauseOnHover: true,
+
+        // The marquee is visible initially positioned next to the border towards it will be moving
+        startVisible: false
+
+    });
+
 })(jQuery);
